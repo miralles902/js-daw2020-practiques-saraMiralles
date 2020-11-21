066b23563d99727d751a6409f1735be7e84aeb31
miralles902 <miralles902@gmail.com>
2020-11-21 11:57:04 +0100

    index html canviat, añadits nous archius per als exercicis i creats els exercicis d'objectes

--- scripts/practica1Obj.js
+++ scripts/practica1Obj.js
@@ -1,23 +1,40 @@
-function objetos_p1() {
-  let punto = new Object();
-  punto.x = 12;
-  punto.y = 5;
+function Punto(x, y) {
+  this.x = isNaN(x) ? 0 : x; // si la coordenada no es un numero es camvia per 0
+  this.y = isNaN(y) ? 0 : y;
+  this.mostrar = () => `(${this.x},${this.y})`;
 
-  punto.cambiar = function () {
+  this.cambiar = function () {
     this.x *= 2;
     this.y *= 5;
-    return `(${punto.x},${punto.y})`;
+    return `(${this.x},${this.y})`;
   };
 
-  punto.suma = function () {
+  this.suma = function () {
     return (this.x += this.y);
   };
 
-  punto.copia = function () {
-    return `(${punto.x},${punto.y})`;
-  };
-
-  console.log(punto.copia());
-  console.log(punto.cambiar());
-  console.log(punto.suma());
+  this.copia = () => new Punto(this.x, this.y);
 }
+
+//creacio dels objectes a y b
+let a = new Punto(10, 20);
+let b = new Punto(a, 21);
+
+//objectes amb una funcio asignada
+var cop = a.copia();
+var cmb = a.cambiar();
+var sum = a.suma();
+
+var cop2 = b.copia();
+var cmb2 = b.cambiar();
+var sum2 = b.suma();
+
+//coordenades amb 2 numeros
+console.log(cop);
+console.log(cmb);
+console.log(sum);
+
+//coordenades amb una lletra convertida en 0
+console.log(cop2);
+console.log(cmb2);
+console.log(sum2);
--- scripts/practica2Obj.js
+++ scripts/practica2Obj.js
@@ -1,7 +1,5 @@
-function objetos_p2() {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-  console.log(arr.reduce(reducer));
-}
+console.log(arr.reduce(reducer) / 8);
