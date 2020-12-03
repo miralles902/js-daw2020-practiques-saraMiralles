6b4e2679970b9d35e2271b81a925bfe5c91782d5
miralles902 <miralles902@gmail.com>
2020-12-03 02:10:03 +0100

    Añadidas actividades de eventos

--- scripts/practica1Events.js
+++ scripts/practica1Events.js
@@ -1,7 +1,7 @@
-document
-  .getElementById('imagen')
-  .addEventListener('keyup', function (keyF12, altkey) {
-    /*utilitzem keyup ya que funcionará 
-    cuant soltem les dos tecles y li pasem les tecles a utilitzar en la funció */ this.innerHTML =
-      '<img src="https://source.unsplash.com/random" />'; //afegim el src de la imatge per a que es vesga
-  });
+document.addEventListener('keyup', function (e) {
+  /*utilitzem keyup ya que funcionará 
+    cuant soltem les dos tecles y li pasem les tecles a utilitzar en la funció */
+  if (e.altKey == true && e.code == 'F12') {
+    this.innerHTML = '<img src="https://source.unsplash.com/random" />'; //afegim el src de la imatge per a que es vesga
+  }
+});
--- scripts/practica2Events.js
+++ scripts/practica2Events.js
@@ -2,14 +2,19 @@ document.getElementById('capa1').addEventListener('dragstart', function (e) {
   e.dataTransfer.setData('text', e.target.id); //guardem el element que arrastrem
   this.style.opacity = '0.5'; //afegim una opacitat del 50%
 });
-document.getElementById('capa2').addEventListener('dragover', function (e) {
-  e.preventDefault(); // Evitem que el cursor apareixca com a no permitit
-  this.style.background = 'red'; //pintem de roig el fondo cuant toquem la capa2
+document.getElementById('capa2').addEventListener('dragenter', capa, false);
+function capa() {
+  e.preventDefault(); // evitem que la pagina fasga cambis no desitjats
+  document.getElementById('capa2').style.background = 'red'; //pintem de roig el fondo cuant toquem la capa2
+}
+document.getElementById('capa2').addEventListener('dragleave', function (e) {
+  e.preventDefault(); // evitem que la pagina fasga cambis no desitjats
+  this.removeEventListener('dragenter', capa); //Eliminem el color roig al eixir del cuadrat
 });
 document.getElementById('capa2').addEventListener('drop', function (e) {
-  e.preventDefault(); //detindre el event pot recargar la pagina, codig per a evitaro
+  e.preventDefault(); // evitem que la pagina fasga cambis no desitjats
   this.style.background = 'yellow'; //pintem el fondo de capa2 de groc
   this.innerHTML = '¡¡¡Lo has logrado!!!'; //cambiem el text
-  let data = e.dataTransfer.getData('text'); //obtenim la informació de capa1
-  document.getElementById(data).style.display = 'none'; //cambiem la visibilitat de capa1 per a que no es vesga
+  let dataT = e.dataTransfer.getData('text'); //obtenim la informació de capa1
+  document.getElementById(dataT).style.display = 'none'; //cambiem la visibilitat de capa1 per a que no es vesga
 });
