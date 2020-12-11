38338743f726c24165296c37f67cccd67bc1476e
miralles_660 <miralles902@gmail.com>
2020-12-11 21:09:14 +0100

    acabats el exercicis conceptes avansats

--- scripts/practica1conAvansats.js
+++ scripts/practica1conAvansats.js
@@ -1,14 +1,14 @@
-var promesa = new Promise((resolver, rechazar) => {
-  setTimeout(() => resolver("Tiempo concluido"), 5000);
-  setTimeout(() => rechazar("El tiempo no va bien"), 10000);
+var promesa = new Promise((resolver, rechazar) => { //creem una nova promesa
+  setTimeout(() => resolver("Tiempo concluido"), 5000); //li diguem que volem que dure 5 segons
+  setTimeout(() => rechazar("El tiempo no va bien"), 10000); // en cas de no funcionar, li diguem que seran 10 segons
 });
 
 promesa
-  .then((respuesta) => {
+  .then((respuesta) => { //si va tot correcte realitzara el seguent codi
     let text = document.getElementById("texto");
     return text.innerHTML ="<p>" + respuesta + "</p>";
   })
-  .catch((error) => {
+  .catch((error) => { //si hi ha un error es para el funcionament y executa
     let text = document.getElementById("texto");
     text.innerHTML = "<p>" + error + "</p>";
   });
--- scripts/temporitzador.js
+++ scripts/temporitzador.js
@@ -1,31 +1,39 @@
-function temporitzador(t) {
-  var promesa = new Promise((resolver, rechazar) => {
-    setTimeout(() => resolver("Tiempo concluido"), t);
-    setTimeout(() => rechazar("El tiempo no va bien"), 10000);
+function temporitzador(intervalo,id) { //asi introduirem el temps en milisegons que volem
+  var promesa = new Promise((resolve, reject) => { //creem la promesa
+    setTimeout(() => resolve("Tiempo concluido"), intervalo); //si funciona correctament
+    setTimeout(() => reject("El tiempo no va bien"), 10000); //si es pasa de temps
   });
-
   promesa
-    .then((respuesta) => {
-      let text = document.getElementById("texto");
+    .then((respuesta) => { //si va tot correcte realitzara el seguent codi
+      let text = document.getElementById(id); //li pasarem el id per parametre
       return (text.innerHTML = "<p>" + respuesta + "</p>");
     })
-    .catch((error) => {
-      let text = document.getElementById("texto");
+    .catch((error) => { //si hi ha un error es para el funcionament y executa
+      let text = document.getElementById(id);
       text.innerHTML = "<p>" + error + "</p>";
     });
 }
 
-async function cuenta(id, i,intervalo) {
-    var counter = id;
-    var id = setInterval(function () {
-      if (counter == 0) {
-        clearInterval(id);
-        cuenta(60, 1);
-      } else {
-        counter = counter - i;
-        document.body.innerHTML = counter;
+async function cuenta(num,i,intervalo,id) { //afegim els parametres
+  /*
+  - num = element inicial del conter enrere
+  - i = numero de que anem a restar, es a dir es llevara de un en un
+  - intervalo = temps en milisegons empleat durant el conter enrere
+  - id = li asignem el id del html 
+  */
+  var contar = num;
+  return await new Promise(resolve => { 
+    const interval = setInterval(() => {
+      if (counter==0) {
+        resolve('Fin');
+        clearInterval(num); //cuant arribem a 0 parara la execucio del programa
       }
-    }, intervalo);
-  }
-
-export{temporitzador,cuenta}
+      else{
+        contar=contar-i; //mentres no pare anirem restant de 1 en 1
+        let text = document.getElementById(id); //li pasarem el id per parametre
+        text.innerHTML = "<p>" + contar + "</p>";
+      }
+    }, intervalo); // asi anira el interval de temps que li asignem
+  });
+}
+export{temporitzador,cuenta} // exportem les dos funcions
